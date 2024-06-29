aaaa3e76b91739beadd147217d1e2a3128508a26
aBBasNMr <nayefnemernn@gmail.com>
2024-06-29 21:32:33 +0300

    Creating constant sound map

--- game.js
+++ game.js
@@ -14,4 +14,10 @@ document.addEventListener('keypress', () => {
     started = true;
   }
 });
-
+const soundMap = {
+  "green": new Audio("sounds/green.mp3"),
+  "red": new Audio("sounds/red.mp3"),
+  "yellow": new Audio("sounds/yellow.mp3"),
+  "blue": new Audio("sounds/blue.mp3"),
+  "wrong": new Audio("sounds/wrong.mp3")
+};
